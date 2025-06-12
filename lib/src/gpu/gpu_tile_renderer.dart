0e6ae51d3992eb3d05ec0452400b07c4b3fa50b3
James Green <james@epicrideweather.com>
2025-06-12 12:48:17 -0700

    background layer

--- lib/src/gpu/gpu_tile_renderer.dart
+++ lib/src/gpu/gpu_tile_renderer.dart
@@ -3,7 +3,11 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart' as m;
 import 'package:flutter_gpu/gpu.dart' as gpu;
+import 'package:vector_tile_renderer/src/themes/theme_layers.dart';
+import 'package:vector_math/vector_math.dart' as vm;
 
+import '../../vector_tile_renderer.dart';
+import '../themes/expression/expression.dart';
 import '../themes/theme.dart';
 import '../tileset.dart';
 import 'color_extension.dart';
@@ -16,28 +20,25 @@ import 'shaders.dart';
 ///
 class GpuTileRenderer {
   final Theme theme;
+  final Logger logger;
 
-  GpuTileRenderer({
-    required this.theme
-  });
+  GpuTileRenderer({required this.theme, Logger? logger})
+      : logger = logger ?? const Logger.noop();
 
-  void render({
-    required ui.Canvas canvas,
-    required ui.Rect clip,
-    required double zoomScaleFactor,
-    required double zoom,
-    required double rotation,
-    required Tileset tile
-  }) {
-
-
+  void render(
+      {required ui.Canvas canvas,
+      required ui.Rect clip,
+      required double zoomScaleFactor,
+      required double zoom,
+      required double rotation,
+      required Tileset tile}) {
     final effectiveTheme = theme.atZoom(zoom);
-
 
     final texture = gpu.gpuContext.createTexture(
         gpu.StorageMode.devicePrivate, clip.width.toInt(), clip.height.toInt());
     final renderTarget = gpu.RenderTarget.singleColor(gpu.ColorAttachment(
-        texture: texture, clearValue: m.Colors.lightBlue.vector4));
+        texture: texture,
+        clearValue: _getBackgroundColor(effectiveTheme.layers.first, zoom)));
 
     final commandBuffer = gpu.gpuContext.createCommandBuffer();
     final renderPass = commandBuffer.createRenderPass(renderTarget);
@@ -71,6 +72,18 @@ class GpuTileRenderer {
     commandBuffer.submit();
     final image = texture.asImage();
     canvas.drawImage(image, ui.Offset.zero, ui.Paint());
+  }
+
+  vm.Vector4 _getBackgroundColor(ThemeLayer baseLayer, double zoom) {
+    if (baseLayer is BackgroundLayer) {
+      final color = baseLayer.fillColor.evaluate(EvaluationContext(
+              () => {}, TileFeatureType.background, logger,
+          zoom: zoom, zoomScaleFactor: 1.0, hasImage: (_) => false));
+      if (color != null) {
+        return color.vector4;
+      }
+    }
+    return m.Colors.orange.vector4;
   }
 
   /// Must call to release resources when done.
